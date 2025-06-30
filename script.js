46b048003e584310acc09930d586684fb3ff3a4e
Elisabetta <156212818+EliMarago@users.noreply.github.com>
2025-06-30 19:04:06 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const container = document.getElementById("container");
+const colors = ["#e74c3c", "#8e44ad", "#3498db", "#e67e22", "#2ecc71"];
+
+const SQUARE = 500;
+
+for (let i = 0; i < SQUARE; i++) {
+  const square = document.createElement("div");
+  square.classList.add("square");
+
+  square.addEventListener("mouseover", () => setColor(square));
+  square.addEventListener("mouseout", () => removeColor(square));
+
+  container.appendChild(square);
+}
+function setColor(el) {
+  const color = getRandomColor();
+  el.style.background = color;
+  el.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+}
+
+function removeColor(el) {
+  el.style.background = "#1d1d1d";
+  el.style.boxShadow = `0 0 2px #1d1d1d`;
+}
+
+function getRandomColor() {
+  return colors[Math.floor(Math.random() * colors.length)];
+}
